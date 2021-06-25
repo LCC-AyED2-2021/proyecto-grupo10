081b3a4eafc23be2ad14bfb1be7823ede51dd2aa
Lucas Bloise <lucasbloise1@gmail.com>
2021-06-25 18:24:10 -0300

    Add command argumets and more

--- personal_library.py
+++ personal_library.py
@@ -3,17 +3,40 @@ from algo1 import *
 from mydicctionary import *
 from mylinkedlist import *
 from utilities import *
+from objets import *
+from serialization import *
 import os
+import sys
 
 print("Bienvenido a la Personal Library de TobyLucas")
-#path = os.chdir('C:\Lucas\Prueba Algoritmos')
-path = os.chdir(r'C:\Users\Tobias\Documents\Facultad\Segundo año\AED 2\documentos algo2')
+path = os.chdir('C:\Lucas\Prueba Algoritmos')
+#path = os.chdir(r'C:\Users\Tobias\Documents\Facultad\Segundo año\AED 2\documentos algo2')
 
 result = insertWordsHash(path)
 firstD = result[0]
-
 totalWords = result[1]
-print("tw", totalWords)
+
+
+#if(sys.argv[1] == "-create"):
+ #   print("Iniciando Create")
+    #pathParamater = sys.argv[2]
+    #path = os.chdir(pathParamater)
+    #ejecutar create
+    #inverted = invertStructure(firstD)
+    #result_to_save = HashAndWords(firstD,10)
+    #hacer_escritura(result_to_save)
+    #persisto el hash en disco
+
+#elif(sys.argv[1] == "-search"):
+ #   print("Iniciando Search")
+    #searchQuery = sys.argv[2]
+    #result_from_lecture = hacer_lectura()
+    #print(result_from_lecture.words)
+    #ejecutar search
+#else:
+ #   print("Ingrese un comando valido :)")
+
+
 
 ##FUNCION HASH
 def hash(key):
@@ -64,8 +87,8 @@ def searchNEW(inverted, word):
 
     return None
 
-inverted = invertStructure(firstD)
-newList = searchNEW(inverted, "for")
-print("NEXT")
-imprimirlista(newList)
+#inverted = invertStructure(firstD)
+#newList = searchNEW(inverted, "for")
+#print("NEXT")
+#imprimirlista(newList)
 #printD(inverted)
