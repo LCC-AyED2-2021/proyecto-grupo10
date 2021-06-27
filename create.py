c9a3f534d500688e2c81a40063586fdfed2c6e5c
Lucas Bloise <lucasbloise1@gmail.com>
2021-06-27 19:00:04 -0300

    Modify Create.py file

--- create.py
+++ create.py
@@ -19,8 +19,6 @@ def hash(key,totalWords):
 
 #Función que desde el primer hash, crea un segundo hash con cada palabra apuntando a un archivo con la cantidad de veces que se encuentra dicha palabra en el archivo
 def invertStructure(S,totalWords):
-    slotsQ = 0
-
     inverted = Array(totalWords, dictionary())
 
     for i in range(0, len(S)):
@@ -29,16 +27,75 @@ def invertStructure(S,totalWords):
 
         while currentNode != None:
             slot = hash(currentNode.value,totalWords)
+            
 
             if inverted[slot] == None: #si el slot está vacío, agregamos la palabra y el nombre del archivo (este ultimo en la cabecera)
                 L = dictionary()
                 addToDictionary(L, currentNode.value, slot)
                 addToDictionary(L, S[i].head.value, slot)
                 inverted[slot] = L
-            else: #si no está vacío, le sumamos a la cabecera (donde tenemos el nombre del archivo), 1 (esto significa que se repite la palabra en ese archivo)
+                
+            elif inverted[slot].head.value == S[i].head.value:
                 inverted[slot].head.repetitions = inverted[slot].head.repetitions + 1
+
+            else:
+                addToDictionary(inverted[slot], S[i].head.value, slot)
+
             
             currentNode = currentNode.nextNode
 
+
+
     print("succesful!")
     return inverted
+
+def insertOnStructure(D, slot, word):
+    if word != '':
+        if D[slot] == None:
+            L = LinkedList()
+            add(L, word)
+            D[slot] = L
+        else:
+            add(D[slot], word)
+
+
+def insertWordsHash(path):
+    files = os.listdir(path)
+    
+    global totalWords #variable que guarda el total de palabras entre todos los textos, aunque se repitan
+    totalWords = 0
+    
+    firstDictionary = Array(len(files), LinkedList())
+    j = 0
+
+    for file in files:
+        f = open(file, encoding="utf8")
+        lines = f.readlines()
+
+        for line in lines:
+            word = ''
+            lentghL = len(line)
+            for i in range(0,lentghL):
+                
+                if line[i] != '.' and line[i] != ',' and line[i] != ' ' and line[i] != '(' and line[i] != ')' and line[i] != '"' and line[i] != '\n':
+                    word = word + line[i]
+
+                    if i == lentghL - 1:
+                        insertOnStructure(firstDictionary, j, word)
+                        totalWords = totalWords + 1
+
+                else:
+                    
+                    if i < lentghL - 2 and ((line[i] == '.' and line[i+1] != ' ') or (line[i] == ',' and line[i+1] != ' ')):
+                        word = word + line[i]
+
+                    insertOnStructure(firstDictionary, j, word)
+
+                    totalWords = totalWords + 1
+                    word = ''
+                
+        insertOnStructure(firstDictionary, j, file)
+        j = j + 1
+    
+    print("succesful!")
+    return firstDictionary, totalWords
